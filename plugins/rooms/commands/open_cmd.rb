39a7e193e0c037b5f37a70d7cb0cafc0d086c98b
The Stranjer <791672+TheStranjer@users.noreply.github.com>
2018-08-11 22:22:19 +0000

    OPEN is now open for intradreamscape connections

--- plugins/rooms/commands/open_cmd.rb
+++ plugins/rooms/commands/open_cmd.rb
@@ -3,13 +3,14 @@ module AresMUSH
     class OpenCmd
       include CommandHandler
 
-      attr_accessor :name
-      attr_accessor :dest
-        
+      attr_accessor :name, :dest, :room, :matched_rooms
+
       def parse_args
         args = cmd.parse_args(ArgParser.arg1_equals_optional_arg2)
         self.name = trim_arg(args.arg1)
-        self.dest = trim_arg(args.arg2)
+        self.dest = trim_arg(args.arg2).to_s
+        self.matched_rooms = Rooms.find_destination(self.dest, enactor)
+        self.room = matched_rooms.first
       end
       
       def required_args
@@ -17,21 +18,27 @@ module AresMUSH
       end
       
       def check_can_build
-        return t('dispatcher.not_allowed') if !Rooms.can_build?(enactor)
-        return nil
+        return if Rooms.can_build?(enactor) || enactor.room.room_owner == enactor.id
+        t('dispatcher.not_allowed')
       end
       
+      def check_same_area
+        return if enactor.is_admin? || room.nil?
+        t('lucidity.different_area') if room.area.id != enactor.room.area.id
+      end
+
+      def check_room_exists
+        t('db.object_not_found') if room.nil?
+      end
+
+      def check_only_one_room_found
+        t('db.object_ambiguous') if matched_rooms.count > 1
+      end
+
       def handle
-        room = nil
-        if (self.dest)
-          matched_rooms = Rooms.find_destination(self.dest, enactor)
-          if (matched_rooms.count != 1)
-            client.emit_failure matched_rooms.count == 0 ? t('db.object_not_found') : t('db.object_ambiguous')
-            return
-          end
-          room = matched_rooms.first
+        enactor.expend(Global.read_config('lucidity', 'costs', 'create_intradreamscape_exit')) do
+          client.emit_success Rooms.open_exit(self.name, enactor_room, room)
         end
-        client.emit_success Rooms.open_exit(self.name, enactor_room, room)
       end
     end
   end
