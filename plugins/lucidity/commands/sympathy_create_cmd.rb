fbae148353dab4d8bfdfed6e1fa3bcfc014d6831
TheStranjer <791672+TheStranjer@users.noreply.github.com>
2018-08-22 07:43:09 +0000

    Fix bug when user doesn't give a target in SYMPATHY/ESTABLISH

--- plugins/lucidity/commands/sympathy_create_cmd.rb
+++ plugins/lucidity/commands/sympathy_create_cmd.rb
@@ -9,11 +9,15 @@ module AresMUSH
       def parse_args
         @target = cmd.args
         @char = Character.find_one_by_name(target)
-        @required_lucidity = Global.read_config("lucidity", "costs", "sympathetic_connection") * (char.sympathies.count + enactor.sympathies.count + 1) * (char.trespassing_resistance_from(enactor) + 1)
+        @required_lucidity = Global.read_config("lucidity", "costs", "sympathetic_connection") * (char.sympathies.count + enactor.sympathies.count + 1) * (char.trespassing_resistance_from(enactor) + 1) if char.present? && enactor.present?
       end
       
+      def check_target_is_used
+        t('dispatcher.invalid_syntax', :cmd => 'sympathies') if target.nil?
+      end
 
       def check_target_exists_and_is_online
+        return if target.nil?
         t('db.no_char_online_found', :name => target) if char.nil? || char.client.nil?
       end
 
